5d2e9529a6f069f116e5e1549fad95b87519a8ce
Liutsing <liutsingluo@163.com>
2023-03-07 17:33:37 +0800

    fix: timeline数据展示

--- packages/admin/src/views/timeline/index.vue
+++ packages/admin/src/views/timeline/index.vue
@@ -16,18 +16,18 @@
       <a-timeline>
         <a-timeline-item
           color="green"
-          v-if="notTodayRecords?.length"
+          v-for="record in notTodayRecords"
         >
           <p class="text-green-500">
-            {{ notTodayRecords[0].date }}
+            {{ record[0].date }}
           </p>
           <p
-            v-for="record in notTodayRecords"
-            :key="record.content"
+            v-for="item in record"
+            :key="item.content"
             class="mb-0"
           >
-            <a-tag color="rgb(34 197 94)">#{{ capitalize(record.type) }}</a-tag
-            >{{ record.time }}: {{ record.content }}
+            <a-tag color="rgb(34 197 94)">#{{ capitalize(item.type) }}</a-tag
+            >{{ item.time }}: {{ item.content }}
           </p>
         </a-timeline-item>
         <a-timeline-item v-if="todayRecords?.length">
@@ -56,13 +56,14 @@ import { fetchList } from './api'
 import { Timeline } from './type'
 import dayjs from 'dayjs'
 import { capitalize } from '@liutsing/utils'
+import { groupBy, values } from 'lodash-es'
 
 const searchKey = 'Timeline-list'
 
 const { query } = useRoute()
 const { page } = query as { page?: number }
 const current = ref<number>(page || 1)
-const pageSize = ref<number>(10*100)
+const pageSize = ref<number>(10 * 100)
 //@ts-ignore
 const type = ref<Timeline['type'] | undefined>('')
 const handleChange = (e: Timeline['type']) => {
@@ -90,6 +91,9 @@ const { isLoading, data } = useQuery<BaseList<Timeline>>(
 )
 
 const currentDate = dayjs().format('dddd, MMMM D, YYYY')
-const notTodayRecords = computed(() => data.value?.records.filter(({ date }) => date !== currentDate))
+const notTodayRecords = computed(() => {
+  const records = data.value?.records.filter(({ date }) => date !== currentDate)
+  return values(groupBy(records, 'date'))
+})
 const todayRecords = computed(() => data.value?.records.filter(({ date }) => date === currentDate))
 </script>
