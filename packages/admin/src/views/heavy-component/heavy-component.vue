7114ba72f620ee85485b091086b356c82906501c
Liutsing Robert <liutsingluo@gmail.com>
2023-09-22 21:20:50 +0800

    wip: use defer

---
+++ packages/admin/src/views/heavy-component/heavy-component.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="item-container">
+    <div
+      class="item"
+      v-for="n in 5000"
+      :key="n"
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.item-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 3px solid #f40;
+}
+.item {
+  width: 5px;
+  height: 5px;
+  background: #ccc;
+  margin: 0.1em;
+}
+</style>
---
+++ packages/admin/src/views/heavy-component/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HeavyComponent from './heavy-component.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div
+      v-for="n in 100"
+      :key="n"
+    >
+      <HeavyComponent />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1em;
+}
+</style>
