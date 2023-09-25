5851da8c2f3fd57d914b248f176e611802150963
Liutsing Robert <liutsingluo@gmail.com>
2023-09-25 21:38:04 +0800

    feat: 广度优先树

--- packages/admin/src/views/binary-draws/README.md
+++ packages/admin/src/views/binary-draws/README.md
@@ -2,3 +2,7 @@
 
 - 递归
 - canvas api
+
+## Resources
+
+- [源码参考](https://github.com/antfu/100/blob/main/src/pages/013.vue)
--- packages/admin/src/views/binary-draws/index.vue
+++ packages/admin/src/views/binary-draws/index.vue
@@ -16,15 +16,19 @@ interface Branch {
   length: number
 }
 const deltaTheta = 0.3
-const length = 40
-const threshold = 0.6
+const length = 5
+const threshold = 0.5
+const minDepth = 4
 function getEndPoint(b: Branch): Point {
   return { x: b.start.x + b.length * Math.cos(b.theta), y: b.start.y + b.length * Math.sin(b.theta) }
 }
 function drawBranch(b: Branch) {
   lineTo(b.start, getEndPoint(b))
 }
-
+/**
+ * 使用任务队列的方式来实现广度优先
+ */
+const pendingTasks: Function[] = []
 function lineTo(p1: Point, p2: Point) {
   ctxRef.value?.beginPath()
   ctxRef.value?.moveTo(p1.x, p1.y)
@@ -41,19 +45,47 @@ const init = () => {
   }
   step(branch)
 }
-function step(branch: Branch) {
+function step(branch: Branch, depth = 0) {
   const end = getEndPoint(branch)
+
+  if (end.x < 0 || end.x > WIDTH) return
+  if (end.y < 0 || end.y > HEIGHT) return
   drawBranch(branch)
 
-  if (Math.random() < threshold) {
-    const leftBranch: Branch = { start: end, length: branch.length, theta: branch.theta - deltaTheta }
-    step(leftBranch)
+  if (depth < minDepth || Math.random() < threshold) {
+    const leftBranch: Branch = {
+      start: end,
+      length: branch.length + (Math.random() * 10 - 5),
+      theta: branch.theta - deltaTheta * Math.random(),
+    }
+    pendingTasks.push(() => step(leftBranch, depth + 1))
   }
-  if (Math.random() < threshold) {
-    const rightBranch: Branch = { start: end, length: branch.length, theta: branch.theta + deltaTheta }
-    step(rightBranch)
+  if (depth < minDepth || Math.random() < threshold) {
+    const rightBranch: Branch = {
+      start: end,
+      length: branch.length + (Math.random() * 10 - 5),
+      theta: branch.theta + deltaTheta * Math.random(),
+    }
+    pendingTasks.push(() => step(rightBranch, depth + 1))
   }
 }
+
+function frame() {
+  const clone = [...pendingTasks]
+  pendingTasks.length = 0
+  clone.forEach((fn) => fn())
+}
+let frameCount = 0
+const startFrame = () => {
+  requestAnimationFrame(() => {
+    frameCount += 1
+    if (frameCount % 10 === 0) frame()
+    startFrame()
+  })
+}
+
+startFrame()
+
 onMounted(() => {
   init()
 })
