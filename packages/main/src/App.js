bbc0317e484a08df1cd38c88c853a82ae6c44e87
Liutsing Robert <liutsingluo@gmail.com>
2024-02-01 17:08:39 +0800

    ~

--- packages/main/src/App.js
+++ packages/main/src/App.js
@@ -5,19 +5,17 @@ import './index.css'
 const App = () => {
   const keys = Object.keys(apps).filter((k) => k !== 'main')
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <aside style={{ width: 160, background: '#fff' }}>
-        <nav>
-          <ul>
-            {keys.map((k) => (
-              <li key={k}>
-                <a href={`http://localhost:${apps['main'].port}/${k}`}>{k}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+    <div className="flex h-screen">
+      <aside className="bg-white w-[160px]">
+        <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 pl-2">
+          {keys.map((k) => (
+            <li key={k}>
+              <a href={`http://localhost:${apps['main'].port}/${k}`}>{k}</a>
+            </li>
+          ))}
+        </ul>
       </aside>
-      <main style={{ flex: 1, marginLeft: 16, background: '#fff' }}>
+      <main className="flex-1 ml-4 h-screen">
         <h1>Main APP</h1>
         <div id="container"></div>
       </main>
