fe178eff773a2ee72f98b42ab5f83f133bc3b3db
Liutsing Robert <liutsingluo@gmail.com>
2025-01-07 14:55:01 +0800

    feat: add rsbuild/rspack example

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: '@liutsing/eslint-config',
-  ignorePatterns: ['*.js'],
+  ignorePatterns: ['*.js', 'packages/rspack', 'packages/rsbuild'],
   rules: {
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
