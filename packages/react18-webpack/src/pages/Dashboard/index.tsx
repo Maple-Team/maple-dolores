2180c045dc08bf4646d30af01a3e4572200aa118
Liutsing Robert <liutsingluo@gmail.com>
2025-02-07 10:36:03 +0800

    feat: update daskboard

--- packages/react18-webpack/src/pages/Dashboard/index.tsx
+++ packages/react18-webpack/src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import { Button, Divider, Space, message } from 'antd'
 import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import dayjs from 'dayjs'
+import { getWeekNumber2, padStart } from '@liutsing/utils'
 import { Progress } from './progress'
 import { useNotificationStore } from '@/stores/notifications'
 
@@ -36,6 +37,9 @@ export default () => {
   return (
     <div>
       <h1 className="py-4 text-[24px]"> {t('current-year-progress', { year: current.format('YYYY') })}</h1>
+      <h2 className="py-4 text-[24px]">
+        {t('current-week-number', { week: padStart(getWeekNumber2(new Date()), 2, '0') })}
+      </h2>
       <Progress />
       <Divider />
       {/* <p>{t('title', { name: 'John' })}</p> */}
