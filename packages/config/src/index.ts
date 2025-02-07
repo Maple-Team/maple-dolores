70e7967ceaf0a462662360c19f8ffe882d8e3308
Liutsing Robert <liutsingluo@gmail.com>
2025-02-07 19:58:09 +0800

    feat: 增加配置的库

---
+++ packages/config/src/index.ts
@@ -0,0 +1,17 @@
+import portMap from './config.json'
+
+type PortMapKey = keyof typeof portMap
+export const isDevelopment = process.env.NODE_ENV !== 'production'
+// TODO
+export const getPublicPath = (appName: PortMapKey) => {
+  const port = portMap[appName].port
+  //   const publicPath = portMap[appName].publicPath
+  return `http://localhost:${port}/`
+}
+
+export const getPort = (appName: string) => {
+  const name: PortMapKey = appName.replace(/@liutsing\//, '') as PortMapKey
+  return portMap[name].port
+}
+
+export const configs = portMap
